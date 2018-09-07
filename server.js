ea2190ceecee89b8a68eaf59f271ee0a0adce764
Mattias Mex <MattiasMex@MacBook-Air-de-Mattias.local>
2018-09-07 16:33:05 +0200

    Server reverse

--- server.js
+++ server.js
@@ -231,21 +231,6 @@ app.use(function(req, res, next){
   res.status(404).send('Page introuvable !');
 });
 
-io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
-    });
-    socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    });
-});
-
-
-
 
 //launch server (listening on port: 8080)
 app.listen(8080);
